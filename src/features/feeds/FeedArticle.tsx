bd14f6a8a3ffda82ad4dcd5961859345dd0f952c
Mathieu Changeat <mchangeat@dydu.ai>
2025-10-23 16:03:34 +0200

    chore(rebase): resolve FeedArticle & codacy config; prefer pnpm

--- src/features/feeds/FeedArticle.tsx
+++ src/features/feeds/FeedArticle.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { ArticleToolbar, ArticleViewMode } from "./ArticleToolbar"
 
 import { FeedItem } from "@/backends/types"
